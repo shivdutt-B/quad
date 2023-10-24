09a5f4591e6ad05de47ba5fb5b83caf4ac109c7a
shivdutt bhadakwad <shivdutt059@gmail.com>
2023-10-24 10:53:51 +0530

    initial commit

---
+++ src/GeneralJs/GeneateLoadingScreen.js
@@ -0,0 +1,70 @@
+function loadingMovies() {
+    let exoSeries = [];
+    for (let i = 0; i < 10; i++) {
+        exoSeries.push(
+            <div style={{ width: 250}} className="display-series-item info-to-store exo-series-element">
+                <div className="display-series-poster exo-series-element-poster"></div>
+            </div>
+        )
+    }
+    return (
+        <div className="display-series-container">
+            <div className="display-series-heading">
+                <div className="display-series-heading-title">
+                    Movie
+                </div>
+            </div>
+            <Slider {...ContextItems.movieAndSeriesSettings}>
+                {exoSeries}
+            </Slider>
+        </div>
+    )
+}
+
+function loadingSeries() {
+    let exoSeries = [];
+    for (let i = 0; i < 10; i++) {
+        exoSeries.push(
+            <div style={{ width: 250}} className="display-series-item info-to-store exo-series-element">
+                <div className="display-series-poster exo-series-element-poster"></div>
+            </div>
+        )
+    }
+    return (
+        <div className="display-series-container">
+            <div className="display-series-heading">
+                <div className="display-series-heading-title">
+                    Series
+                </div>
+            </div>
+            <Slider {...ContextItems.movieAndSeriesSettings}>
+                {exoSeries}
+            </Slider>
+        </div>
+    )
+}
+
+function genEle() {
+    let screenArea = window.innerWidth * window.innerHeight
+    let boxDimension = 240 * 180
+    let nrOfBox = Math.floor(screenArea / boxDimension)
+    const arrayOfElements = [];
+    for (let i = 0; i < nrOfBox; i++) {
+        arrayOfElements.push(
+            <div className="loading-screen-element-container">
+                <div className="loading-screen-element-poster">
+
+                </div>
+                <div className="loading-screen-element-desc">
+                    <div className="loading-screen-element-desc-title"></div>
+                    <div className="loading-screen-element-desc-rating"></div>
+                </div>
+            </div>
+        );
+    }
+    return (
+        <div className="movie-container-loading">
+            {arrayOfElements}
+        </div>
+    )
+}
